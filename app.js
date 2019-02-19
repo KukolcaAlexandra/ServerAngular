fc5545d0608a94cb1ea5cc9ec09ab4d70abbd269
Alexandra Kukolca <Alexandra_Kukolca1@epam.com>
2019-02-19 10:23:02 +0300

    clean code

--- app.js
+++ app.js
@@ -96,18 +96,15 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 app.use(function (req, res, next) {
-  console.log('middleware');
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-  //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  //res.setHeader('Access-Control-Allow-Origin', 'https://epam-education.herokuapp.com');
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  //res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Credentials', true);
   // Pass to next layer of middleware
   next();
 });
